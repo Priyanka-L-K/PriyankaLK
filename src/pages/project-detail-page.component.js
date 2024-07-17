da6968d342ab2fd6f0b0bc28524662fbf88d66d8
Priyanka Lakur Krishnamurthy <67971841+Priyanka-L-K@users.noreply.github.com>
2024-07-17 00:53:47 -0500

    Update project-detail-page.component.js

--- src/pages/project-detail-page.component.js
+++ src/pages/project-detail-page.component.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useParams, Link } from "react-router-dom";
 import "../css/global.css";
 import "../css/project-detail.css";
@@ -9,29 +9,21 @@ import { LuCircleDot } from "react-icons/lu";
 import LinkBadge from "../components/link-badge.component";
 
 import TagList from '../components/tag-list.component';
-import {getProjectCategoryIndex} from "../Utils/utility";
-
+import { getProjectCategoryIndex } from "../Utils/utility";
 
 const ProjectDetail = () => {
 
     const { id, projectType } = useParams();
     const { companyId, companyProjectType, projectId } = useParams();
-    const languages = ["python", "C++"]
-    const tools = ["git"]
-    const libraries = ["pathlib"]
 
-    const project_index = id? getProjectCategoryIndex(projectsData.all_projects, projectType) : getProjectCategoryIndex(works.works[companyId].all_projects, companyProjectType)
+    const project_index = id ? getProjectCategoryIndex(projectsData.all_projects, projectType) : getProjectCategoryIndex(works.works[companyId].all_projects, companyProjectType);
 
-    const project = id ? projectsData.all_projects[project_index].projects[id] : works.works[companyId].all_projects[0].projects[projectId]
+    const project = id ? projectsData.all_projects[project_index].projects[id] : works.works[companyId].all_projects[0].projects[projectId];
 
     useEffect(() => {
         // Scroll to the top once the component mounts
         window.scrollTo(0, 0);
-    
-        // Or, for a smooth scrolling effect (if supported by the browser)
-        // window.scrollTo({ top: 0, behavior: 'smooth' });
-      }, []);
-
+    }, []);
 
     return (
         <div className="parent-div page-top-padding">
@@ -41,29 +33,23 @@ const ProjectDetail = () => {
 
                 <div className="d-flex justify-content-between ">
                     <div className="heading">
-
                         {project.heading}
                         <div className="date--down">  {project.startDate}</div>
 
-
-                        <div className="section-content">
-
-                        </div>
+                        <div className="section-content"></div>
                         <div className="d-flex flex-wrap">
-                        {project.links.map((link) => {
-                            return <LinkBadge
-                                websiteLink={link.website_link}
-                                linkname={link.linkname}
-                                icon={link.icon}
-                                color={link.color}
-                            />
-                        })}
-
+                            {project.links.map((link, index) => {
+                                return <LinkBadge
+                                    key={index}
+                                    websiteLink={link.website_link}
+                                    linkname={link.linkname}
+                                    icon={link.icon}
+                                    color={link.color}
+                                />
+                            })}
                         </div>
-           
                     </div>
                     <div className="date--beside">  {project.startDate}</div>
-
                 </div>
 
                 <div className="pt-2">
@@ -72,28 +58,35 @@ const ProjectDetail = () => {
                     <TagList tags={project.libraries} className="libraries-tag" />
                 </div>
 
-
                 <div className="pt-2 section-content">
                     <div className="section-sub-heading">Key features</div>
-
-                    {project.features.map((feature) => {
-                        return <div>
-                            <span> <LuCircleDot /> </span> &nbsp;
-                            <span dangerouslySetInnerHTML={{ __html: feature }} />
-                        </div>
-
+                    {project.features.map((feature, index) => {
+                        return (
+                            <div key={index}>
+                                <span> <LuCircleDot /> </span> &nbsp;
+                                <span dangerouslySetInnerHTML={{ __html: feature }} />
+                            </div>
+                        );
                     })}
                 </div>
 
-                <div className='d-flex justify-content-center'>
-                    <img className="project-image" src={project.main_image} />
+                <div className="pt-2 section-content">
+                    <div className="section-sub-heading">Project Images</div>
+                    {project.images.map((image, index) => {
+                        return (
+                            <div key={index} className="d-flex flex-column align-items-start mb-4">
+                                <div className="image-description mb-2">{image.description}</div>
+                                <img className="project-image" src={image.url} alt={image.description} />
+                            </div>
+                        );
+                    })}
                 </div>
+
                 <div className="section-sub-heading">
                     Project Impact
                 </div>
 
                 <div className="section-content">
-
                     {project.project_impact}
                 </div>
             </div>
@@ -101,4 +94,4 @@ const ProjectDetail = () => {
     )
 }
 
-export default ProjectDetail
+export default ProjectDetail;
